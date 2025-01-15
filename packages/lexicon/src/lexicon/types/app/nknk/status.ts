73a1ffc89cda93bb98484bd3722e2ce2a40a87a7
Zerry Hogan <dev@jerrywithaz.com>
2025-01-15 09:46:23 -0600

    Add lexicon package

---
+++ packages/lexicon/src/lexicon/types/app/nknk/status.ts
@@ -0,0 +1,25 @@
+/**
+ * GENERATED CODE - DO NOT MODIFY
+ */
+import { ValidationResult, BlobRef } from '@atproto/lexicon'
+import { lexicons } from '../../../lexicons'
+import { isObj, hasProp } from '../../../util'
+import { CID } from 'multiformats/cid'
+
+export interface Record {
+  status: string
+  createdAt: string
+  [k: string]: unknown
+}
+
+export function isRecord(v: unknown): v is Record {
+  return (
+    isObj(v) &&
+    hasProp(v, '$type') &&
+    (v.$type === 'app.nknk.status#main' || v.$type === 'app.nknk.status')
+  )
+}
+
+export function validateRecord(v: unknown): ValidationResult {
+  return lexicons.validate('app.nknk.status#main', v)
+}
