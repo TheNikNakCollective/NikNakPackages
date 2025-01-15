2484d0af036cabf5759b0c7cf8fab39f5324756d
Zerry Hogan <dev@jerrywithaz.com>
2025-01-15 11:14:36 -0600

    Marshall auth state and session

--- packages/orm/src/repositories/auth-session/model.ts
+++ packages/orm/src/repositories/auth-session/model.ts
@@ -1,21 +1,11 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm'
 import type { NodeSavedSession } from '@atproto/oauth-client-node'
-import { ValueTransformer } from 'typeorm'
-
-export const transformer: ValueTransformer = {
-    to: (value: NodeSavedSession | undefined): string | undefined => {
-        return value ? JSON.stringify(value) : undefined
-    },
-    from: (value: string | undefined): NodeSavedSession | undefined => {
-        return value ? JSON.parse(value) : undefined
-    },
-}
 
 @Entity('auth_sessions')
 export class AuthSession {
     @PrimaryColumn()
     key!: string
 
-    @Column({ type: 'json', nullable: false, transformer })
+    @Column({ type: 'json', nullable: false })
     session!: NodeSavedSession
 }
--- packages/orm/src/repositories/auth-state/model.ts
+++ packages/orm/src/repositories/auth-state/model.ts
@@ -1,21 +1,11 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm'
 import type { NodeSavedState } from '@atproto/oauth-client-node'
-import { ValueTransformer } from 'typeorm'
-
-export const transformer: ValueTransformer = {
-    to: (value: NodeSavedState | undefined): string | undefined => {
-        return value ? JSON.stringify(value) : undefined
-    },
-    from: (value: string | undefined): NodeSavedState | undefined => {
-        return value ? JSON.parse(value) : undefined
-    },
-}
 
 @Entity('auth_states')
 export class AuthState {
     @PrimaryColumn()
     key!: string
 
-    @Column({ type: 'json', nullable: false, transformer })
+    @Column({ type: 'json', nullable: false })
     state!: NodeSavedState
 }
